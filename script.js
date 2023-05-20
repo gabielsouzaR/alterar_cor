e549c1c810bf206999a1f2b1e7dc4315aa01bbbb
gabielsouzaR <souza.ramos@aluno.ifsp.edu.br>
2023-05-20 01:53:44 -0300

    novo

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function alterarCor() {
-    const corInput = document.getElementById('corInput');
-    const cor = corInput.value;
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    document.body.style.backgroundColor = cor;
+    // Simulação de autenticação (usuário: admin, senha: admin)
+    if (username === 'admin' && password === 'admin') {
+        window.location.href = 'home.html'; // Redireciona para a página principal
+    } else {
+        alert('Usuário ou senha incorretos');
+    }
+}
+
+function alterarCor(cor) {
+    if (cor) {
+        document.body.style.backgroundColor = cor;
+    } else {
+        const nomeCorInput = document.getElementById('nomeCorInput');
+        const codigoCorInput = document.getElementById('codigoCorInput');
+        const nomeCor = nomeCorInput.value;
+        const codigoCor = codigoCorInput.value;
+
+        if (codigoCor) {
+            document.body.style.backgroundColor = codigoCor;
+        } else if (nomeCor) {
+            document.body.style.backgroundColor = nomeCor;
+        }
+    }
 }
